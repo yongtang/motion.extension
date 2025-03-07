4a3fdc1302ac32045607904481a877fc4edba391
Yong Tang <yong.tang.github@outlook.com>
2025-03-07 04:44:52 +0000

    Add websocket

--- motion/extension.py
+++ motion/extension.py
@@ -5,7 +5,7 @@ from . import websocket
 
 class MotionExtension(omni.ext.IExt):
     def on_startup(self, ext_id: str):
-        self.server = asyncio.create_task(
+        self.server = asyncio.get_event_loop().create_task(
             uvicorn.Server(
                 uvicorn.Config(
                     websocket.app,
@@ -19,12 +19,13 @@ class MotionExtension(omni.ext.IExt):
         print("[MotionExtension] Extension startup")
 
     def on_shutdown(self):
-        if getattr(self, "server", None):
+        if getattr(self, "server", None) and not self.server.done():
             self.server.cancel()
             try:
-                await self.server
+                asyncio.get_event_loop().run_until_complete(self.server)
             except asyncio.CancelledError:
-                pass
+                print("[MotionExtension] Server shutdown")
+            except RuntimeError as e:
+                print("[MotionExtension] Server exception: {}".format(e))
             self.server = None
-            print("[MotionExtension] Server shutdown")
         print("[MotionExtension] Extension shutdown")

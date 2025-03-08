e86004c91c8b0e9e5d79f0e00d5c699438fbff3f
Yong Tang <yong.tang.github@outlook.com>
2025-03-08 23:04:13 +0000

    Fix

--- motion/extension.py
+++ motion/extension.py
@@ -52,7 +52,6 @@ class MotionExtension(omni.ext.IExt):
             if (
                 getattr(self, "server_task")
                 and self.server_task
-                and not self.server_task.done()
             ):
                 try:
                     self.server_task.cancel()
